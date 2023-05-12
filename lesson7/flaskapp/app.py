05b1eef6dd7ecd4179af201017af7970037bba27
saba070201 <saba07022001@yandex.ru>
2023-05-12 16:56:13 +0300

    m7f

--- lesson7/flaskapp/app.py
+++ lesson7/flaskapp/app.py
@@ -11,13 +11,32 @@ db=SQLAlchemy()
 db.init_app(app)
 @app.route('/',methods=['GET', 'POST'])
 def home():
-      notes=db.session.execute(db.select(Notes)).scalars()
+      notes=db.session.execute(db.select(Notes).order_by(Notes.important.desc())).scalars()
       return render_template('home.html',notes=notes)
 
 @app.route('/note-<int:note_pk>',methods=['GET', 'POST'])
 def note(note_pk):
       note=db.get_or_404(Notes,note_pk)
       return render_template('note.html',note=note)
-
+@app.route('/create-note')
+def create_note():
+     note=Notes(title='title',memo='memo',important=1)
+     db.session.add(note)
+     db.session.commit()
+     return redirect(url_for('note',note_pk=note.id))
+@app.route('/delete-note-<int:note_pk>')
+def delete_note(note_pk):
+     note=db.get_or_404(Notes,note_pk)
+     db.session.delete(note)
+     db.session.commit()
+     return redirect(url_for('home'))
+@app.route('/change-note-<int:note_pk>',methods=['GET', 'POST'])
+def change_note(note_pk):
+     note=db.get_or_404(Notes,note_pk)
+     if request.method=='POST':
+        note.title=request.form.get('title')
+        note.memo=request.form.get('memo')
+        db.session.commit()
+        return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True) # debug = True очень важен !!!!!! и if __name__ на всякий тоже надо!!!!
